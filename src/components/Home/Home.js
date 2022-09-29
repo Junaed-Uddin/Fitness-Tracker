db37cba6b6b0689d0f71662466b14d3aaa01ae97
Junaed-Uddin <junaed.mohammed190@gmail.com>
2022-09-29 18:54:53 +0600

    some modification

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import ExerciseDetails from '../ExerciseDetails/ExerciseDetails';
 import ExerciseList from '../ExerciseList/ExerciseList';
 import './Home.css';
+import gym from '../../gym.png';
 
 const Home = () => {
     const [equipments, setEquipments] = useState([]);
@@ -23,8 +24,11 @@ const Home = () => {
         <div>
             <div className='flex flex-wrap sm:flex-nowrap'>
                 <div className=''>
-                    <h2 className='text-3xl text-sky-500 mt-14 mb-5 ml-5 sm:ml-10 text-start font-bold'>Fitness Tracker</h2>
-                    <p className='text-start text-white font-bold mb-10 text-xl ml-5 sm:ml-10'>Choose Self Burning Exercise</p>
+                    <div className='mt-14 flex mb-5 sm:ml-5'>
+                        <img className='w-24' src={gym} alt="" />
+                        <h2 className='text-3xl text-sky-500 mt-8 text-start font-bold'>Fitness Tracker</h2>
+                    </div>
+                    <p className='text-start text-white font-bold mb-5 text-xl ml-5 md:ml-8 lg:ml-11'>Choose Self Burning Exercise</p>
                     <div className='exerciseList grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 sm:mr-5 lg:ml-10 ml-3 sm:ml-5 mr-3'>
                         {
                             equipments.map(equipment => <ExerciseList key={equipment.id} equipment={equipment} exerciseTime={() => addToCart(equipment)}></ExerciseList>)
